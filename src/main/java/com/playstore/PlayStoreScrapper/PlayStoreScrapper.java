4c26b7845c7b4a75128b0f0c253f8e8c186ae341
Alejandro Garcia Hernandez <alejandro.inves@gmail.com>
2024-06-04 16:00:05 +0200

    Maven Java config

---
+++ src/main/java/com/playstore/PlayStoreScrapper/PlayStoreScrapper.java
@@ -0,0 +1,12 @@
+package com.playstore.PlayStoreScrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication()
+public class PlayStoreScrapper {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayStoreScrapper.class, args);
+	}
+
+}
